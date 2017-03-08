89c8c87381c9100d5b9bfe858f694d78cc330b52
Noppadet Vivatchotikul <Noppadet Vivatchotikul>
2017-03-08 19:50:46 +0700

    add work code

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-from readSudoku import *
+from sudoku import *
+from copy import copy, deepcopy
+
+
+def solving(position_x, position_y, table):
+    for i in range(1, 10):
+        clone_table = deepcopy(table)
+        clone_table[position_y][position_x] = str(i)
+        print("edit position" + str(position_x) + "," + str(position_y))
+        print_table(table)
+        print("=======")
+        print_table(clone_table)
+        print("ddddddddddddddddddd")
+        if check_table(clone_table, position_x, position_y):
+            temp_position = find_next(position_x, position_y, clone_table)
+            print(temp_position)
+            solving(temp_position[0], temp_position[1], clone_table)
+        # else:
+        #     table = clone_table
+
+
+def find_next(position_x, position_y, table):
+    if not valid_position(position_x, position_y):
+        return
+    while 1:
+        print(str(position_x) + str(position_y))
+        if position_x == 0 and position_y == 9:
+            print("done result\n\n")
+            print_table(table)
+            quit()
+        elif table[position_y][position_x] != "0":
+            if position_x == 8:
+                position_y += 1
+                position_x = 0
+            else:
+                position_x += 1
+        else:
+            return [position_x, position_y]
+
 
 data = import_file()
+print("start")
 print_table(data)
-for i in range(0, 9):
-    for j in range(0, 9):
-        print(str(i)+"+"+str(j),end=" => ")
-        print(check_table(data,i,i))
+print()
+position = find_next(0, 0, data)
+solving(position_x=position[0], position_y=position[1], table=data)
+
--- sudoku.py
+++ sudoku.py
@@ -3,35 +3,35 @@ from tkinter.filedialog import askopenfilename
 
 
 def print_table(table):
-    for i in range(0, 9):
+    for i in range(0, 9):  # no. of row.
         for j in range(0, 9):
             print(table[i][j], end=" ")
-            if (j+1) % 3 == 0:
+            if (j + 1) % 3 == 0:
                 print("| ", end="")
         print()
-        if (i+1) % 3 == 0:
-            print("- "*12)
+        if (i + 1) % 3 == 0:
+            print("- " * 12)
 
 
 def check_table(table, x, y):
     # check in block.
     collection_test = [True] * 9
     init_x = x // 3 * 3  # to get point to start iterate row.
-    init_y = y // 3 * 3 # to get point to start iterate column.
+    init_y = y // 3 * 3  # to get point to start iterate column.
     for i in range(0, 3):
         for j in range(0, 3):
-            num = int(table[init_x + i][init_y + j]) - 1
-            if num == -1:
+            num = int(table[init_y + i][init_x + j]) - 1
+            if num == -1:  # empty
                 continue
             elif collection_test[num]:
                 collection_test[num] = False
             else:
                 return False
 
-    # check in row.
+    # check in column.
     collection_test = [True] * 9
     for i in range(0, 9):
-        num = int(table[i][y]) - 1
+        num = int(table[y][i]) - 1
         if num == -1:
             continue
         elif collection_test[num]:
@@ -39,10 +39,10 @@ def check_table(table, x, y):
         else:
             return False
 
-    # check in column.
+    # check in row.
     collection_test = [True] * 9
     for i in range(0, 9):
-        num = int(table[x][i]) - 1
+        num = int(table[i][x]) - 1
         if num == -1:
             continue
         elif collection_test[num]:
@@ -51,6 +51,7 @@ def check_table(table, x, y):
             return False
 
     return True
+
 
 def import_file():
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
@@ -64,10 +65,12 @@ def import_file():
         data[i] = temp[i].split(",")
     return data
 
+
+def valid_position(position_x, position_y):
+    return position_x in range(0, 9) and position_y in range(0, 9)
+
 # print_table(data)
 # for i in range(0, 9):
 #     for j in range(0, 9):
 #         print(str(i)+"+"+str(j),end=" => ")
 #         print(check_table(data,i,i))
-
-
